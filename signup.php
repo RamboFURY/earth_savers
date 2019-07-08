0a87418ed11bdb6ce6c4a2508259a7717747f59e
sahil pathania <sahil.pathania.3979@gmail.com>
2019-07-08 18:16:36 +0530

    6 files in once

--- signup.php
+++ signup.php
@@ -1,20 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
-        <!--jQuery library-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <!--Latest compiled and minified JavaScript-->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Panels in Bootstrap</title>
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-    <body>
+
+ <?php require 'common.php'; ?>
+
         <div class="container">
             <div class="row">
                 <div class="col-xs-4 col-xs-offset-4">
-
+<br><br><br>
                     <div class="panel panel-primary">
                         <div class="panel-heading"><h3>REGISTER</h3></div>
                         <div class="panel-body">
@@ -57,5 +47,3 @@
             </div>
         </div>
     </div>
-</body>
-</html>
