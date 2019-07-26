23003b9c516affbf04b16ade5366b76c73ae6aa9
sahil pathania <sahil.pathania.3979@gmail.com>
2019-07-27 01:10:57 +0530

    added new

--- dashboard.php
+++ dashboard.php
@@ -32,6 +32,39 @@
             </div>
                   </div>
   </div>
+  <br><br>
+
+
+
+  <p><h1>Problem : Waste Disposal Management System</h1></p>
+
+  <p><h1>Solution : YOU</h1></p>
+
+  <center>
+
+    <p><img src="images\1.jpg"  alt="1">
+    <img src="images\2.jpg"  alt="2">
+    <img src="images\3.jpg"  alt="3">
+    <img src="images\4.jpg"  alt="4"></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+              <br><br><br>
+
+                <div class="panel">
+                  <div class="panel-body">
+    <p><strong>Americans generate over 4 pounds of waste per person </strong></p>
+    <p> Totaling more than 220 million tons of trash each year – a majority of which gets sorted and sent to landfills.With more than 3,500 landfills across the country, these dumps create the second largest source of human-related methane emissions in the country. Unfortunately, that number has continued to grow over time, and methane has the potential to trap heat in the atmosphere 25 times more effectively than carbon dioxide.What really happens to the trash we send away, and how long does it take discarded items to decompose once they’ve been tossed out? We studied everything from paper towels and banana peels to plastic bags and disposable diapers. If you’re curious to know how long your 4 pounds of daily trash will take up space in our landfills, explore how much trash is near your home and keep reading. Think about how many paper towels you use every day, or even how many you might grab at one time to help clean up a mess. Every single one of those paper towels takes two weeks to a month to fully decompose from the time you toss it into the bin. That’s enough time to binge-watch every single episode of “Law & Order” (that’s over 450 episodes, if you’ve been keeping track).Fruits take longer to decompose than you might think. An orange or banana peel won’t decompose for two to five weeks, and an apple core can take even longer than that. While you’re waiting for those bananas to break down, you could have enough time to apply for and receive your passport.Now imagine the cotton shirts you wear. Maybe it’s an undergarment, or something you use for yardwork, so you can just throw it away if it gets dirty. Every shirt you’ve ever thrown away could take between 2 and 5 months to decay once it hits the dumpster.
+Plywood for your DIY moments and craft inspirations could take just as long to fully decay as “Toy Story 3” took Pixar to make – nearly three years. What may take even longer? Cigarette filters, which are often absentmindedly discarded on the street, can actually take upward of five full years to decompose.Add these items up and they still don’t take as long to break down as a plastic bag. In certain states, plastic bags continue to be outlawed and take up to 20 years to decay fully. That’s approximately how long it could have taken to build the Great Pyramid of Khufu, at Giza. It’s something to think about the next time you see those 99-cent reusable bags at the checkout lane. Worse still is the time it takes nylon fabric to decay in our landfills: an average of 35 years, which is how long it took to narrow the gender wage gap by 20 cents.</p>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</center>
+
+
 
   <footer class="footer">
     <div class="container">
